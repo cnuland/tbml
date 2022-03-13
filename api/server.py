d8cacfcd6af0f810a13530390a25c9d25cdbd695
C.J. Nuland <cjnuland@gmail.com>
2022-03-13 01:36:04 -0500

    fix cors

--- api/server.py
+++ api/server.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080","https://tbml-api-tbml.apps.okd4.cjlabs.dev"],
+    allow_origins=["http://localhost:8080","https://ibml-app-tbml.apps.okd4.cjlabs.dev","http://ibml-app-tbml.apps.okd4.cjlabs.dev","ibml-app-tbml.apps.okd4.cjlabs.dev"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
