1942e21ee7900b703d94babe1f043529ca77f4e8
C.J. Nuland <cjnuland@gmail.com>
2022-03-13 00:12:56 -0500

    oops need to add https

--- api/server.py
+++ api/server.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=["http://localhost:8080","https://tbml-api-tbml.apps.okd4.cjlabs.dev"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- app/frontend/src/main.js
+++ app/frontend/src/main.js
@@ -7,7 +7,7 @@ import router from './router';
 
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://tbml-api-tbml.apps.okd4.cjlabs.dev';  // the FastAPI backend
+axios.defaults.baseURL = 'https://tbml-api-tbml.apps.okd4.cjlabs.dev';  // the FastAPI backend
 
 Vue.config.productionTip = false;
 
