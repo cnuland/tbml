292c1855257954ccbdc4c028a842227ca01dc516
C.J. Nuland <cnuland@redhat.com>
2022-03-29 14:03:11 -0400

    added the localhost option

--- api/db/config.py
+++ api/db/config.py
@@ -4,7 +4,7 @@ TORTOISE_ORM = {
     "connections": {"default": "postgres://userAP8:MqEy5fvcMnrg4Ulp@postgresql.tbml.svc.cluster.local:5432/sampledb"},
     "apps": {
         "models": {
-            "models": [
+            "models": [n
                 "db.models", "aerich.models"
             ],
             "default_connection": "default"
--- app/frontend/src/main.js
+++ app/frontend/src/main.js
@@ -7,8 +7,8 @@ import router from './router';
 
 
 axios.defaults.withCredentials = true;
-//axios.defaults.baseURL = 'https://tbml-api-tbml.apps.okd4.cjlabs.dev';  // the FastAPI backend
-axios.defaults.baseURL = 'http://localhost:5000';  // the FastAPI backend
+axios.defaults.baseURL = 'https://tbml-api-tbml.apps.okd4.cjlabs.dev';  // the FastAPI backend
+//axios.defaults.baseURL = 'http://localhost:5000';  // the FastAPI backend
 
 Vue.config.productionTip = false;
 
